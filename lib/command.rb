c42ecab2468f607bd4bbd1e7b943ff21a4d8b369
Zhansong Li <zhansongl@ir.com>
2018-11-20 23:19:08 +1100

    basic functionality working

--- lib/command.rb
+++ lib/command.rb
@@ -18,17 +18,36 @@ module VagrantPlugins
         # Get CWD:
         cwd_result = run_powershell_inline_json("(Get-Item -Path '.\\').FullName")
         cwd = cwd_result.stdout
-        puts "Hey, here's the cwd: #{cwd}"
 
         # Get IP Address and switch
         network_result = run_powershell_inline_json("Get-VM | Where-Object {$_.State -eq 'Running'} | Select -ExpandProperty NetworkAdapters | Select VMName, IPAddresses, SwitchName | ConvertTo-Json")
         network = JSON.parse(network_result.stdout)
-        puts "Hey, here's the network JSON: #{network.inspect}"
 
         # Get Configuration Dir
         config_result = run_powershell_inline_json("Get-VM | Where-Object {$_.State -eq 'Running'} | Select VMName, ConfigurationLocation | ConvertTo-Json")
         config = JSON.parse(config_result.stdout)
-        puts "Hey, here's the config JSON: #{config.inspect}"
+
+        # Filter out VMs that are not managed by vagrant in this folder
+        vagrant_vms = {}
+        for vm in config
+          vm_name = vm["VMName"]
+          vm_config_path = vm["ConfigurationLocation"]
+          if vm_config_path.strip.upcase.start_with?(cwd.strip.upcase)
+            vagrant_vms[vm_name] = vm
+          end
+        end
+
+        # add network properties to resulting hash
+        for vm in network
+          vm_name = vm["VMName"]
+          if vagrant_vms.has_key?(vm_name)
+            for key in vm.keys
+                vagrant_vms[vm_name] = vagrant_vms[vm_name].merge(vm)
+            end
+          end
+        end
+
+        puts vagrant_vms.values.inspect
       end
 
       private
